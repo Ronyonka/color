62c8d66b86d9725e844ffa27379afdf6bfcaf11a
Ronyonka <rononyonka@gmail.com>
2019-08-09 15:48:33 +0300

    adding rgb color selection

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ $(function(){
     $('#submit').on('click', function(e){
         var rgb = $('#rgb').val()
         var hex = $('#hex').val()
-        $('body').css('background', '#'+hex)
-        $('.input').css('background', '#'+hex)
-        if(hex == 000){
-            $('.logo').css('color', '#fff')
-            $('.input').css('border-bottom','2px solid #fff')
-            $('.input').css('color', '#fff')
-            $('input').addClass('myClass')
+        if(rgb.length == '' && $('#hex').val().length != ''){
+            $('body').css('background', '#'+hex)
+            $('.input').css('background', '#'+hex)
+            $('#hex').val('');
+            if(hex == 000 || hex == 0000 || hex == 00000 || hex == 000000){
+                $('.logo').css('color', '#fff')
+                $('.input').css('border-bottom','2px solid #fff')
+                $('.input').css('color', '#fff')
+                $('input').addClass('myClass')
+            }
+        }else if(hex.length == '' && rgb.length != ''){
+            $('body').css('background', 'rgb(' +rgb + ')')
+            $('.input').css('background', 'rgb(' +rgb + ')')
+            $('#rgb').val('');
+            if(rgb == 0,0,0){
+                $('.logo').css('color', '#fff')
+                $('.input').css('border-bottom','2px solid #fff')
+                $('.input').css('color', '#fff')
+                $('input').addClass('myClass')
+            }
+        }else if(hex.length != '' && rgb.length == ''){
+
         }
     })
 })
